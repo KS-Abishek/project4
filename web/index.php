c25bcf084e7404bdb11bf3fb8eb637834be74417
abishek <abishek5791@gmail.com>
2020-08-31 15:05:18 +0530

    webfolder commit

---
+++ web/index.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "7.0.0.133";
+$username = "root";
+$password = "Root@!#86675";
+$dbname = "tech";
+$port = "3306";
+
+// Create connection
+$conn = new mysqli($servername, $port, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM techpioneer";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "name: " . $row["name"]. " - salary: " . $row["salary"]. "company_name : " . $row["company_name"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
