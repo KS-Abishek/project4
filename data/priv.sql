388890419d3da0d0833e01dc09792e53b7a3670f
KS-Abishek <67574103+KS-Abishek@users.noreply.github.com>
2020-09-01 13:14:18 +0530

    Update priv.sql

--- data/priv.sql
+++ data/priv.sql
@@ -1,1 +1,4 @@
-UPDATE mysql.user SET Host="%" WHERE User='root';
+alter user 'root'@'localhost' identified with mysql_native_password by '123';
+alter user 'root'@'%' identified with mysql_native_password by '123';
+
+
