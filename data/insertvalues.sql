5a5b3009d12d3c7190097c385dd54e6ca73fbbd0
KS-Abishek <67574103+KS-Abishek@users.noreply.github.com>
2020-09-01 13:32:57 +0530

    Update insertvalues.sql

--- data/insertvalues.sql
+++ data/insertvalues.sql
@@ -1,5 +1,5 @@
-insert into techpioneer values ("Abishek",250000,"dxc");
-insert into techpioneer values ("Eshwar",245000,"dxc");
+insert into techpioneer values ("abc",250000,"dxc");
+insert into techpioneer values ("def",245000,"dxc");
 insert into techpioneer values ("Vinay",246000,"dxc");
 insert into techpioneer values ("Teja",247000,"dxc");
 insert into techpioneer values ("Harshini",248000,"dxc");
